262da1c99edc2750a1b0094e2269fa8f950b2413
kreenaparikh <parikhkreena@gmail.com>
2017-01-07 11:47:35 -0800

    Christmas Tree

---
+++ coding1.py
@@ -0,0 +1,14 @@
+stage.set_background("grid")
+print("*")
+print("*"*2)
+print("*"*3)
+print("*"*4)
+print("*")
+print("*")
+print("*")
+hieght=5
+width=3
+#for x in (hieght):
+#[1,2,3,4]
+#for y in (width):
+#[1,2,3,4]
